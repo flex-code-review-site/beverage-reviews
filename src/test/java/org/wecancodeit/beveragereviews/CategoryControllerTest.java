9ac977b5f851c92d81222152c9f0e3ce3ee06183
carrington-jordan <carrington.jordanb@gmail.com>
2019-03-02 15:46:08 -0500

    Ability to find all categories

---
+++ src/test/java/org/wecancodeit/beveragereviews/CategoryControllerTest.java
@@ -0,0 +1,49 @@
+package org.wecancodeit.beveragereviews;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import org.wecancodeit.beveragereviews.models.Category;
+import org.wecancodeit.beveragereviews.repositories.CategoryRepository;
+
+public class CategoryControllerTest {
+
+	@InjectMocks
+	private CategoryController underTest;
+	
+	@Mock
+	private Category catone;
+	
+	@Mock
+	private Category cattwo;
+	
+	@Mock
+	private Model model;
+	
+	@Mock
+	private CategoryRepository categoryRepo;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldAddAllCategoriesToModel() {
+		Collection<Category> allCategories = Arrays.asList(catone,cattwo);
+		when(categoryRepo.findAll()).thenReturn(allCategories);
+		
+		underTest.findAllCategories(model);
+		verify(model).addAttribute("categories", allCategories);
+		
+	}
+}
