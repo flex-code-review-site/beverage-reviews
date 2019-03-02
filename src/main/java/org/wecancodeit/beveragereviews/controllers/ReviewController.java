11ee128a542cf8a94083edae5d22f98c8a77801d
drewgainey <drewgainey@gmail.com>
2019-03-02 16:41:10 -0500

    ability to find one or find all reviews

--- src/main/java/org/wecancodeit/beveragereviews/controllers/ReviewController.java
+++ src/main/java/org/wecancodeit/beveragereviews/controllers/ReviewController.java
@@ -1,5 +1,29 @@
 package org.wecancodeit.beveragereviews.controllers;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.wecancodeit.beveragereviews.models.Review;
+import org.wecancodeit.beveragereviews.repositories.ReviewRepository;
+
+@Controller
 public class ReviewController {
+
+	private ReviewRepository reviewRepo;
+
+	public String findAllReviews(Model model) {
+		model.addAttribute("reviews", reviewRepo.findAll());
+		return "reviews";
+	}
+
+	public String findOneReview(Long id, Model model) {
+		Optional<Review> review = reviewRepo.findById(id);
+		if (review.isPresent()) {
+			model.addAttribute("review", review.get());
+		}
+		return "review";
+
+	}
 
 }
