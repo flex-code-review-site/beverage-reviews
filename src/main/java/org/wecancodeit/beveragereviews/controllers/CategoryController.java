da6a6e13739643025dca0880e9c92b4a41dfad5a
quincyrogers <quincyrogers>
2019-03-03 11:58:14 -0500

    created findOne category

--- src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryController.java
+++ src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryController.java
@@ -1,6 +1,9 @@
 package org.wecancodeit.beveragereviews.controllers;
 
+import java.util.Optional;
+
 import org.springframework.ui.Model;
+import org.wecancodeit.beveragereviews.models.Category;
 import org.wecancodeit.beveragereviews.repositories.CategoryRepository;
 
 public class CategoryController {
@@ -10,6 +13,16 @@ public class CategoryController {
 	public String findAllCategories(Model model) {
 		model.addAttribute("categories", categoryRepo.findAll());
 		return "categories";
+
+	}
+
+	public String findOneCategory(long id, Model model) {
+		Optional<Category> category = categoryRepo.findById(id);
+		if (category.isPresent()) {
+			model.addAttribute("category", category.get());
+
+		}
+		return "category";
 
 	}
 
