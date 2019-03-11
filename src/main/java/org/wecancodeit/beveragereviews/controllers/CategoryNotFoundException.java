df5f8b30658064876200933f104c75a08a099ace
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-11 14:28:30 -0400

    added all missing review controller mvc tests and associated
    edited file causing conflict

--- src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryNotFoundException.java
+++ src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryNotFoundException.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5672898804549428389L;
 
 }
