3b748634f7704a24c70fb9fd2492781a01df61dd
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-11 14:43:01 -0400

    added @Resonse Status annotation
    in category not found exception in master to resolve merge conflict

--- src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryNotFoundException.java
+++ src/main/java/org/wecancodeit/beveragereviews/controllers/CategoryNotFoundException.java
@@ -1,5 +1,9 @@
 package org.wecancodeit.beveragereviews.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends Exception {
 
 	private static final long serialVersionUID = -5672898804549428389L;;
