342221f3f82cffd666917fd9980d7d9b0b17dc8b
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-11 14:31:03 -0400

    added all missing review controller mvc tests and associated
    adding something related to merge conflicts

--- src/main/java/org/wecancodeit/beveragereviews/controllers/TagNotFoundException.java
+++ src/main/java/org/wecancodeit/beveragereviews/controllers/TagNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Actor Not Found")
 public class TagNotFoundException extends Exception {
 
+	private static final long serialVersionUID = -2285405401945477506L;
+
 //	@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Actor Not Found")
 //	public class ActorNotFoundException extends Exception {
 //	    // ...
