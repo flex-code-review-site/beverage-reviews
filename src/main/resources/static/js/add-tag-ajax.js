e4791442483af3732e7b8aa126539c5a8b9147bd
ryanandrewmcpherson <ryanmcphersondev@gmail.com>
2019-03-15 14:57:03 -0400

    finally working add tag ajax!!!

--- src/main/resources/static/js/add-tag-ajax.js
+++ src/main/resources/static/js/add-tag-ajax.js
@@ -2,12 +2,12 @@
 
 const submitTagButton = document.querySelector(".add-tag button");
 const submitTagInput = document.querySelector(".add-tag input");
-const tagsList = document.querySelector(".tags-list ul");
+const tagsList = document.querySelector (".tags-list");
 
-const xhr = new XMLHttpRequest;
+const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
-  if(xhr.readystate === 4 && xhr.status === 200){
-    const res = xhr.responseText;
+  if(xhr.readyState === 4 && xhr.status === 200){
+    const res = xhr.response;
     tagsList.innerHTML = res;
   }
 }
